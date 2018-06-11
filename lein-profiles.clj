81dec0f2367da0a547688b1cf0a1bce39a5740fd
David Lantos <david.lantos@gmail.com>
2018-06-11 09:43:10 +0100

    lein: Upgrade user plugins to latest versions

--- lein-profiles.clj
+++ lein-profiles.clj
@@ -1,3 +1,3 @@
-{:user {:plugins [[lein-ancient "0.6.12"]
-                  [lein-kibit "0.1.5"]
-                  [lein-marginalia "0.9.0"]]}}
+{:user {:plugins [[lein-ancient "0.6.15"]
+                  [lein-kibit "0.1.6"]
+                  [lein-marginalia "0.9.1"]]}}
