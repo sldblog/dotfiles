e44174dc15696a0452d23e9390755669240d7d31
David Lantos <david.lantos@gmail.com>
2025-01-24 10:25:24 +0000

    theme: only add necessary parts to prompt

--- zsh/theme-spaceship.zsh
+++ zsh/theme-spaceship.zsh
@@ -2,4 +2,23 @@ SPACESHIP_TIME_SHOW=true
 SPACESHIP_USER_SHOW=always
 SPACESHIP_GIT_STATUS_SHOW=false
 
-spaceship remove aws
+SPACESHIP_CHAR_SYMBOL="$"
+SPACESHIP_CHAR_SUFFIX=" "
+SPACESHIP_CHAR_SYMBOL_ROOT="#"
+
+SPACESHIP_PROMPT_ORDER=(
+  time
+  user
+  dir
+  git
+  node
+  ruby
+  python
+  elixir
+  golang
+  exec_time
+  line_sep
+  exit_code
+  sudo
+  char
+)
